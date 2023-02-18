da632965da87d6c06b8513c3a51c535a54ff2a1c
DanielGarQ <60907486+DanielGarQ@users.noreply.github.com>
2023-02-17 19:52:02 -0500

    Update main.py
    Se elimina un comentario innecesario

--- main.py
+++ main.py
@@ -21,29 +21,3 @@ elif opcion == 2:
         print("Lo sentimos, esta opcion aun se encuentra en desarrollo")
 
 
-
-
-
-
-
-
-'''def jugador_inicial():
-    print("Antes de jugar podrá elegir si desea que el jugador inicial sea aleatorio o elegido previamente")
-    inicio = int(input("Desea elegir el jugador inicial?: "))
-    print("1).Si")
-    print("2).No\n")
-    if inicio == 1:
-        turno_singleplayer = int(input("Quién jugará primero?\nEl jugador 1 o 2?: "))
-        if turno_singleplayer == 1:
-            pass
-#Empieza el juego
-
-        elif turno_singleplayer == 2:
-            pass
-
-
-
-    elif inicio == 2:
-        print(f"Iniciará el jugador: {random.randint(1, 2)}")
-        #empieza el juego'''
-
